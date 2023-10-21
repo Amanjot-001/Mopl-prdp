62fdb97d1656e204684cb1e3333b911bbb903e3c
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-22 00:45:40 +0530

    more block tests

--- tests/block-test.js
+++ tests/block-test.js
@@ -27,4 +27,55 @@ module.exports = test => {
             }
         ]
     })
+
+
+    // empty block
+    test(`
+    {
+    }`, {
+        type: 'Program',
+        body: [
+            {
+                type: 'BlockStatement',
+                body: []
+            }
+        ]
+    })
+
+    // nested block
+    test(`
+    {
+        23;
+        {
+            "hello";
+        }
+    }`, {
+        type: 'Program',
+        body: [
+            {
+                type: 'BlockStatement',
+                body: [
+                    {
+                        type: 'ExpressionStatement',
+                        expression: {
+                            type: 'NumericLiteral',
+                            value: 23
+                        }
+                    },
+                    {
+                        type: 'BlockStatement',
+                        body: [
+                            {
+                                type: 'ExpressionStatement',
+                                expression: {
+                                    type: 'StringLiteral',
+                                    value: 'hello'
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    })
 }
