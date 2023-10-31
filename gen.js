77c0e4eb06ec59ea6d55426af067fcdac7c27c1c
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-31 12:30:54 +0530

    list added

--- gen.js
+++ gen.js
@@ -5,15 +5,44 @@ class Generator {
 
     generate(node) {
         if (node.type === "Program") {
-            this.ExpressionStatement(node.body);
+            this.StatementList(node.body);
             return this.code;
         } else {
             throw new Error("Unsupported AST node type: " + node.type);
         }
     }
 
+    StatementList(body) {
+        body.map(statement => this.ExpressionStatement(statement));
+    }
+
+    Statement(node) {
+        switch (node.type) {
+            case ';':
+                return this.EmptyStatement();
+            case '{':
+                return this.BlockStatement();
+            case 'let':
+                return this.VariableStatement();
+            case 'if':
+                return this.IfStatement();
+            case 'def':
+                return this.FunctionDeclaration();
+            case 'class':
+                return this.ClassDeclaration();
+            case 'return':
+                return this.ReturnStatement();
+            case 'while':
+            case 'do':
+            case 'for':
+                return this.IterationStatement();
+            default:
+                return this.ExpressionStatement();
+        }
+    }
+
     ExpressionStatement(node) {
-        return this.Expression(node[0].expression);
+        return this.Expression(node.expression);
     }
 
     Expression(node) {
